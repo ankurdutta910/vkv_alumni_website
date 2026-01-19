e69c3b238a7ea64c2987638aa20ecbadfbcf8f9b
ankurdutta910 <103182503+ankurdutta910@users.noreply.github.com>
2026-01-19 19:50:43 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -5,15 +5,7 @@ import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
 var firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBrqsxfpgvnranZURCMdJqcXqvYXtjv1dg",
-  apiKey: "AIzaSyBYu9xgTLz2v63O_Ym8ppmtAMddIcWzonM",
-  authDomain: "kuber-55702.firebaseapp.com",
-  databaseURL: "https://kuber-55702-default-rtdb.firebaseio.com",
-  projectId: "kuber-55702",
-  storageBucket: "kuber-55702.appspot.com",
-  messagingSenderId: "69266128350",
-  appId: "1:69266128350:web:b250f57d6b32527604b14f",
-  measurementId: "G-QTTM5VPJ3J",
+ ******
 });
 
 // Initialize Firebase
@@ -22,3 +14,4 @@ export const auth = getAuth(firebaseApp);
 export const database = getDatabase(firebaseApp);
 export const storage = getStorage(firebaseApp);
 export { db };
+
